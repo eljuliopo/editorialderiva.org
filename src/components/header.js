5f6a52e222fd2590462d7f60bd395b1aeee9c7b2
piducancore:/# <hola@piducan.dev>
2021-12-18 14:10:35 -0300

    add menu

--- src/components/header.js
+++ src/components/header.js
@@ -1,5 +1,5 @@
 /** @jsx jsx */
-import { jsx, Container, Themed } from "theme-ui"
+import { jsx, Container, Themed, Flex } from "theme-ui"
 import { Link } from "gatsby"
 
 const Header = ({ siteTitle }) => (
@@ -9,11 +9,33 @@ const Header = ({ siteTitle }) => (
     }}
   >
     <Container>
-      <Themed.h1>
+      <Themed.h1 sx={{ mb: 0 }}>
         <Themed.a as={Link} to="/">
           {siteTitle}
         </Themed.a>
       </Themed.h1>
+      <Flex>
+        <Themed.h4>
+          <Themed.a as={Link} to="/nosotros">
+            nosotros
+          </Themed.a>
+        </Themed.h4>
+        <Themed.h4 sx={{ ml: 3 }}>
+          <Themed.a as={Link} to="/catalogo">
+            catalogo
+          </Themed.a>
+        </Themed.h4>
+        <Themed.h4 sx={{ ml: 3 }}>
+          <Themed.a as={Link} to="/contacto">
+            contacto
+          </Themed.a>
+        </Themed.h4>
+        <Themed.h4 sx={{ ml: 3 }}>
+          <Themed.a as={Link} to="/carrito">
+            carrito
+          </Themed.a>
+        </Themed.h4>
+      </Flex>
     </Container>
   </header>
 )
