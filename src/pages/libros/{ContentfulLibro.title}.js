b24bf9a36d8838285a108271021cb62378f91ece
piducancore:/# <hola@piducan.dev>
2021-12-18 12:50:53 -0300

    clean up

--- src/pages/libros/{ContentfulLibro.title}.js
+++ src/pages/libros/{ContentfulLibro.title}.js
@@ -1,12 +1,13 @@
 /** @jsx jsx */
-import { jsx, Grid, Box, Themed, Button } from "theme-ui"
+import { jsx, Themed, Button } from "theme-ui"
+import { graphql } from "gatsby"
 import { GatsbyImage } from "gatsby-plugin-image"
 import { MDXRenderer } from "gatsby-plugin-mdx"
-import { graphql } from "gatsby"
-import { useCart } from "../../store"
 
 import Layout from "../../components/layout"
 import Seo from "../../components/seo"
+
+import { useCart } from "../../store"
 
 export default function Libro(props) {
   const { addToCart } = useCart()
@@ -20,43 +21,23 @@ export default function Libro(props) {
         image={current.image.gatsbyImageData.images.fallback.src}
         author={current.authors}
       />
-      <Grid columns={[1, "1fr 2.4fr"]} variant="primary">
-        <Box
-          sx={{
-            position: [null, "sticky"],
-            alignSelf: [null, "start"],
-            top: [null, 56],
-          }}
-        >
-          <GatsbyImage
-            image={current.image.gatsbyImageData}
-            alt={current.title}
-            objectFit="contain"
-            sx={{ mx: "auto", cursor: "pointer" }}
-          />
-        </Box>
-        <Box>
-          <Themed.h2>{current.title}</Themed.h2>
-          <Themed.h5 sx={{ mt: 1 }}>por {current.authors[0]}</Themed.h5>
-          <Themed.h3>${current.price}</Themed.h3>
-          <MDXRenderer>{current.description.childMdx.body}</MDXRenderer>
-          <Themed.p>ISBN: {current.isbn} </Themed.p>
-          <Themed.p>{current.pages} páginas</Themed.p>
-          <Themed.p>
-            {current.height} x {current.width} cm.
-          </Themed.p>
-          <Themed.p>{current.year}</Themed.p>
-          <div
-            sx={{
-              display: "flex",
-              flexDirection: ["column", "column", "row"],
-              alignItems: ["stretch", "stretch", "center"],
-            }}
-          >
-            <Button onClick={() => addToCart(current)}>Agregar al carro</Button>
-          </div>
-        </Box>
-      </Grid>
+      <GatsbyImage
+        image={current.image.gatsbyImageData}
+        alt={current.title}
+        objectFit="contain"
+        sx={{ mx: "auto", cursor: "pointer" }}
+      />
+      <Themed.h2>{current.title}</Themed.h2>
+      <Themed.h5 sx={{ mt: 1 }}>por {current.authors[0]}</Themed.h5>
+      <Themed.h3>${current.price}</Themed.h3>
+      <MDXRenderer>{current.description.childMdx.body}</MDXRenderer>
+      <Themed.p>ISBN: {current.isbn} </Themed.p>
+      <Themed.p>{current.pages} páginas</Themed.p>
+      <Themed.p>
+        {current.height} x {current.width} cm.
+      </Themed.p>
+      <Themed.p>{current.year}</Themed.p>
+      <Button onClick={() => addToCart(current)}>Agregar al carro</Button>
     </Layout>
   )
 }
