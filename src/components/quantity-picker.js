4a4df7f0fc90016ef3ba887e900c94c662dbb4bf
piducancore:/# <hola@piducan.dev>
2021-12-19 16:13:08 -0300

    update qty picker

--- src/components/quantity-picker.js
+++ src/components/quantity-picker.js
@@ -3,39 +3,57 @@ import { jsx, Themed, Button } from "theme-ui"
 
 import { useCart } from "../store"
 
+const pickerButtonsStyles = {
+  width: 20,
+  height: 20,
+  display: "flex",
+  justifyContent: "center",
+  alignItems: "center",
+  p: 0,
+  border: ({ colors }) => `solid 1px ${colors.primary}`,
+  borderRadius: 0,
+}
+
 export default function QtyPicker({ item, ...props }) {
   const { increment, decrement } = useCart()
   return (
-    <div sx={{ display: "flex", alignItems: "center" }} {...props}>
-      <Button
+    <div
+      sx={{
+        display: "flex",
+        alignItems: "center",
+        border: ({ colors }) => `solid 1px ${colors.primary}`,
+      }}
+      {...props}
+    >
+      <Themed.p
         sx={{
-          width: 32,
-          height: 32,
-          borderRadius: 360,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
+          m: 0,
+          textAlign: "center",
+          width: 40,
         }}
-        onClick={() => decrement(item)}
       >
-        {"-"}
-      </Button>
-      <Themed.p sx={{ m: 0, textAlign: "center", width: 40 }}>
         {item.quantity}
       </Themed.p>
-      <Button
-        sx={{
-          width: 32,
-          height: 32,
-          borderRadius: 360,
-          display: "flex",
-          alignItems: "center",
-          justifyContent: "center",
-        }}
-        onClick={() => increment(item)}
-      >
-        {"+"}
-      </Button>
+      <div sx={{ display: "flex", flexDirection: "column" }}>
+        <Button
+          sx={{
+            ...pickerButtonsStyles,
+            border: ({ colors }) => `solid 0.5px ${colors.background}`,
+          }}
+          onClick={() => increment(item)}
+        >
+          {"+"}
+        </Button>
+        <Button
+          sx={{
+            ...pickerButtonsStyles,
+            border: ({ colors }) => `solid 0.5px ${colors.background}`,
+          }}
+          onClick={() => decrement(item)}
+        >
+          {"-"}
+        </Button>
+      </div>
     </div>
   )
 }
